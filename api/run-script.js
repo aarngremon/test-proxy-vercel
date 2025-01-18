0dac8017e833cd6f4130fc231ebdb2e6366ff074
sklbz <sokol.bozanic.sklbz@gmail.com>
2025-01-18 16:32:12 +0100

    update

--- api/run-script.js
+++ api/run-script.js
@@ -1,7 +1,23 @@
 import { exec } from 'child_process';
 
+
+
 export default function handler(_, res) {
-	exec('bash script.sh', (error, stdout, stderr) => {
+	const code = `
+		git config user.name "sklbz"
+		git config user.email "sokol.bozanic.sklbz@gmail.com"
+
+		git pull
+
+		echo "Bash script executed at $(date)" >>log.txt
+		cat .txt >>log.txt
+
+		git add .
+		git commit -m "check"
+		git push
+	`;
+
+	exec(code, { shell: '/bin/bash' }, (error, stdout, stderr) => {
 		if (error) {
 			console.error(`Error: ${error.message}`);
 			return res.status(500).json({ error: error.message });
