bd93d76450c752d94cf7988136be7384386b75c8
sklbz <sokol.bozanic.sklbz@gmail.com>
2025-01-18 16:01:11 +0100

    update

--- api/run-script.js
+++ api/run-script.js
@@ -4,13 +4,14 @@ export default function handler(_, res) {
 	exec('bash script.sh', (error, stdout, stderr) => {
 		if (error) {
 			console.error(`Error: ${error.message}`);
-			return;
+			return res.status(500).json({ error: error.message });
 		}
 		if (stderr) {
 			console.error(`stderr: ${stderr}`);
-			return;
+			return res.status(500).json({ error: stderr });
 		}
 		console.log(`stdout: ${stdout}`);
+		return res.status(200).json({ output: stdout })
 	});
 }
 
